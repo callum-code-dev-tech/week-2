dd3f0e0b8707eb66f44bf65b46565de5b2f456c4
callum-code-dev-tech <callumwithington2006@gmail.com>
2026-01-12 13:10:24 +0000

    added js code

---
+++ app.js
@@ -0,0 +1,17 @@
+console.log(`For some reason this is not working`)
+
+const userWord = prompt(`pick a word`)
+console.log(userWord)
+
+const yourFriendsWord = `hi`
+
+function alertTheUser(word) {
+    alert(`your word was ${word}...`)
+}
+
+function add7AndF(f){
+    console.log(7+f)
+}
+
+add7AndF(13)
+add7AndF(98)
