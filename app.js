81e6fd5676fadf3eebba1c3235fd28f799abf104
callum-code-dev-tech <callumwithington2006@gmail.com>
2026-01-12 14:57:24 +0000

    added new title

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ console.log(`For some reason this is not working`)
 const userWord = prompt(`pick a word`)
 console.log(userWord)
 
-const yourFriendsWord = `hi`
+const yourFriendsWord = ("hi")
 
 function alertTheUser(word) {
     alert(`your word was ${word}...`)
 }
+alertTheUser(userWord)
+
 
 function add7AndF(f){
     console.log(7+f)
@@ -15,3 +17,27 @@ function add7AndF(f){
 
 add7AndF(13)
 add7AndF(98)
+
+function figuresOutCSScore(h, k) {
+    const myFigure = 890
+
+    return h + k + myFigure
+
+}
+
+const userCSScore = figuresOutCSScore(8, 11)
+console.log(userCSScore)
+
+function seventeen(){
+    return 17
+}
+
+function eightytwo(){
+    return 82
+}
+
+function twelve(){
+    return 12
+}
+
+console.log(seventeen() / twelve() * eightytwo())
